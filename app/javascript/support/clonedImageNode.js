d01ee1b2f2d0a03a2d6e4eb865ea063eaaf136c3
puskuruk <tamayeseruysal@gmail.com>
2019-10-09 06:37:07 +0300

    Created shared function for created slides for slide links

---
+++ app/javascript/support/clonedImageNode.js
@@ -0,0 +1,12 @@
+import slideItemsLength from "../support/slideItemsLength";
+
+const clonedImageNode = () =>{
+    const slideItems = await slideItemsLength();
+    const willClonedImage = document.getElementById("will_cloned_image");
+    const clonedImage = willClonedImage.cloneNode(true);
+    clonedImage.removeAttribute("id");
+    clonedImage.classList.add(slideItems);
+    document.getElementById("uploadPreview").append(clonedImage);
+}
+
+export default clonedImageNode;
