029bd71b90fb3f00191f7b9e8e3e1cf6f97ac88d
puskuruk <tamayeseruysal@gmail.com>
2019-10-09 07:12:34 +0300

    We can create new slides programmatically with add new slide link

--- app/javascript/controllers/slides_controller.js
+++ app/javascript/controllers/slides_controller.js
@@ -1,8 +1,11 @@
 import { Controller } from "stimulus"
+import clonedImageNode from "../support/clonedImageNode";
+import addSlideLink from "../support/addSlideLink";
 
 export default class SlidesController extends Controller {
     addSlide(event){
         event.preventDefault();
-        console.log("clicked");
+        clonedImageNode();
+        addSlideLink();
     }
 }
